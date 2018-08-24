0e55915d56564e95c4d58591474a82f274e53dac
fullstack-dev <hall0711@hotmail.com>
2018-08-24 08:39:35 +0100

    - updated mongo db url

--- config/database.config.js
+++ config/database.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    url: 'mongodb://fullstack-dev:forthefuture0711@ds125932.mlab.com:25932/fullstackdatabase'
+    url: 'mongodb://fullstack-dev:forthefuture0711 @ds125932.mlab.com:25932/fullstackdatabase'
 }
